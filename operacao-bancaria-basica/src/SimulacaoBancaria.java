d99a0eefb6e8cfb846fd44dcbb3b4e493484e293
Antonioafj <antonioafj.edu@gmail.com>
2024-05-06 08:59:12 -0400

    codigo passou nos testes

--- operacao-bancaria-basica/src/SimulacaoBancaria.java
+++ operacao-bancaria-basica/src/SimulacaoBancaria.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 
 public class SimulacaoBancaria {
-
     public static void main(String[] args) {
-      
            /* System.out.println("1. Depositar");
             System.out.println("2. Sacar");
             System.out.println("3. Consultar Saldo");
@@ -12,18 +10,22 @@ public class SimulacaoBancaria {
         Scanner scanner = new Scanner(System.in); 
         double saldo = 0; 
         boolean fim = true;
+
         // Loop infinito para manter o programa em execução até que o usuário decida sair
+
         while (fim) { 
             
             int opcao = scanner.nextInt(); 
+
         // TODO: Implemente as condições necessárias para avalia a opção escolhida:
         // Dica: Utilze o switch/case para o programa realizar as operações escolhidas pelo usuário.
+
         switch (opcao) {
             case 1:{
                 //System.out.println(" Digite O valor de deposito: ");
                 double valorDeposito = scanner.nextDouble();
-                double saldoAtual = saldo + valorDeposito;
-                System.out.println("Saldo Atual : " + saldoAtual);
+                saldo += valorDeposito;
+                System.out.println("Saldo atual: " + saldo);
                 break;
             }
             case 2:{
@@ -32,13 +34,13 @@ public class SimulacaoBancaria {
                 if(valorSacado > saldo){
                   System.out.println("Saldo insuficiente. ");
                 } else {
-                  double saldoNovo = saldo - valorSacado;
-                  System.out.println("Saldo Atual : " + saldoNovo);
+                  saldo -= valorSacado;
+                  System.out.println("Saldo atual: " + saldo);
                 }
                 break;
             }
             case 3:{
-                System.out.println("Saldo Atual " + saldo );
+                System.out.println("Saldo atual: " + saldo );
                 break;
             }
             case 4:{
@@ -56,9 +58,11 @@ public class SimulacaoBancaria {
              default:
                 System.out.println("Programa encerrado.");
                 break;
+
         // Exibe mensagem de opção inválida se o usuário escolher uma opção inválida:    
                   //System.out.println("Opção inválida. Tente novamente.");
                     //System.out.println("Opção inválida. Tente novamente."); 
+
             }
         }
     }
